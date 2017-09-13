08f7a765b68e4a4a606da1a0d52312eaab4986d3
MG <max.gurkivskyi@gmail.com>
2017-09-13 12:37:14 +0300

    Added example code

--- Example/GruveoSampleApp/AppDelegate.m
+++ Example/GruveoSampleApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import GruveoPublicSDK;
 
 @interface AppDelegate ()
 
@@ -16,7 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [GruveoCallManager setClientId:@"demo"];
+    
     return YES;
 }
 
--- Example/GruveoSampleApp/ViewController.m
+++ Example/GruveoSampleApp/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+@import GruveoPublicSDK;
 
-@interface ViewController ()
+@interface ViewController () <GruveoCallManagerDelegate, UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@property (nonatomic, strong) NSURLSession *session;
 
 @end
 
@@ -16,14 +23,83 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self.activityIndicator stopAnimating];
+    [GruveoCallManager setDelegate:self];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.textField becomeFirstResponder];
 }
 
+#pragma mark - Private
+
+- (void)makeCall {
+    if (self.textField.text.length == 0) {
+        return;
+    }
+    self.button.hidden = YES;
+    [self.activityIndicator startAnimating];
+    
+    [self.textField resignFirstResponder];
+
+    [GruveoCallManager callCode:self.textField.text videoCall:YES onViewController:self callCreationCompletion:^(CallInitError creationError) {
+        self.button.hidden = NO;
+        [self.activityIndicator stopAnimating];
+        if (creationError != CallInitErrorNone) {
+            [self.textField becomeFirstResponder];
+            // show error here
+            NSLog(@"callCreationCompletion error: %li", creationError);
+        }
+    }];
+}
+
+- (IBAction)callAction:(id)sender {
+    [self makeCall];
+}
+
+#pragma mark - GruveoCallManagerDelegate
+
+- (void)requestToSignApiAuthToken:(NSString *)token {
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-demo.gruveo.com/signer"]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"text/plain" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:[token dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    if (!self.session) {
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        self.session = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:nil];
+    }
+    
+    [[self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if ([data isKindOfClass:[NSData class]]) {
+            NSString *signedToken = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            [GruveoCallManager authorize:signedToken];
+        } else {
+            [GruveoCallManager authorize:nil];
+        }
+    }] resume];
+}
+
+- (void)callEstablished {
+    NSLog(@"callEstablished");
+}
+
+- (void)callEnd:(GruveoCallEndReason)reason {
+    NSLog(@"callEnd with reason: %li", reason);
+}
+
+- (void)recordingStateChanged {
+    NSLog(@"recordingStateChanged");
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self makeCall];
+    return NO;
+}
 
 @end
